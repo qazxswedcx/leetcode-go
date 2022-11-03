6f74e6369ffa135494a6f9f8aa9385bd4716468d
zxw <1123242050@qq.com>
2022-11-03 14:36:05 +0800

    1668

--- src/1668.最大重复子字符串.go
+++ src/1668.最大重复子字符串.go
@@ -2,10 +2,25 @@ package main
 
 import "fmt"
 
-func maxRepeating(sequence string, word string) int {
-	res := 0
-
-	return res
+func maxRepeating(sequence, word string) (ans int) {
+	n, m := len(sequence), len(word)
+	if n < m {
+		return
+	}
+	f := make([]int, n)
+	for i := m - 1; i < n; i++ {
+		if sequence[i-m+1:i+1] == word {
+			if i == m-1 {
+				f[i] = 1
+			} else {
+				f[i] = f[i-m] + 1
+			}
+			if f[i] > ans {
+				ans = f[i]
+			}
+		}
+	}
+	return
 }
 
 func main() {
